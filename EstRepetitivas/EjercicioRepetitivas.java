42b3b5ca9799c6279f41830fd29f0801b4e2c785
sergiorosas197 <srosasjilapa@gmail.com>
2022-05-10 17:54:56 +0000

    Ejemplo. Repetitivo

---
+++ EstRepetitivas/EjercicioRepetitivas.java
@@ -0,0 +1,21 @@
+package EjercicioRepetitivas;
+
+public class EjercicioRepetitivas {
+
+    public static void ejer41While() {
+        int contador=1;
+        int suma=0;
+        while (contador<=20) {
+            if (contador%2==0) {
+                suma=suma+contador;
+            }
+            contador++;
+        }
+    System.out.println("La respuesta es:"+suma);
+    }
+
+
+    public static void main(String[] args) {
+        ejer41While();
+    }
+}
