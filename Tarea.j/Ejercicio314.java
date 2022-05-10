c8806fb3412aaafb10ba8c60de819bfd795048cc
sergiorosas197 <srosasjilapa@gmail.com>
2022-05-10 01:28:17 +0000

    mi tarea

---
+++ Tarea.j/Ejercicio314.java
@@ -0,0 +1,25 @@
+public class Ejercicio34 {
+    
+    static Scanner lt=new Scanner(System.in);
+    public static void main(String[] args) {
+        //Declarar variables
+    double nota;
+    int numeros;
+    //Datos de entrada
+    System.out.println("Ingrese nota:");
+    nota=lt.nextDouble();
+    System.out.println("Ingrese letra:");
+    numeros=lt.nextInt();
+    //Proceso
+    if (A=10) {
+
+        
+    }
+        
+    }{
+        
+    }
+}
+
+    
+}
