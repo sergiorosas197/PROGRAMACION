e1923ae45fcd92d848461f095cd4ff337b27cc12
sergiorosas197 <srosasjilapa@gmail.com>
2022-05-11 02:33:48 +0000

    tareas

--- Tarea/Ejercicio314.java
+++ Tarea/Ejercicio314.java
@@ -1,9 +1,11 @@
-public class Ejercicio34 {
+import java.util.Scanner;
+
+public class Ejercicio314 {
     
     static Scanner lt=new Scanner(System.in);
     public static void main(String[] args) {
         //Declarar variables
-    double nota;
+    double nota=A;B;C;D;F;
     int numeros;
     //Datos de entrada
     System.out.println("Ingrese nota:");
---
+++ Tarea/ExamenPreg1SLRJ.java
@@ -0,0 +1,30 @@
+package PROGRAMACION;
+import java.util.Scanner;
+
+public class Tarea; {
+
+    public static void nota=10() {
+    System.out.println("Si la nota es 10 es:A");
+    }
+    public static void nota=9() {
+    System.out.println("Si la nota es 9 es:B");
+    }
+    public static void nota=8() {
+    System.out.println("S la nota es 8 es:C");
+    }
+    public public static void nota() {
+        
+    }
+        
+    }
+        
+    }
+
+    
+}
+    
+}() {
+    
+}
+    
+}
